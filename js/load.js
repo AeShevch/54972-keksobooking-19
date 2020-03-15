3f37c6c6af8e3a7cb265718fc745617cdadd312b
AeShevch <shevch.ae@gmail.com>
2020-03-15 16:34:40 +0300

    module6-task2: Исправлены отступы

--- js/load.js
+++ js/load.js
@@ -1,30 +1,30 @@
 'use strict';
 (function () {
-    window.load = function (url, onSuccess, onError) {
-        var TIMEOUT = 10000;
-        var xhr = new XMLHttpRequest();
+  window.load = function (url, onSuccess, onError) {
+    var TIMEOUT = 10000;
+    var xhr = new XMLHttpRequest();
 
-        xhr.responseType = 'json';
+    xhr.responseType = 'json';
 
-        xhr.addEventListener('load', function () {
-            if (xhr.status === 200) {
-                onSuccess(xhr.response);
-            } else {
-                onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-            }
-        });
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
 
-        xhr.addEventListener('error', function () {
-            onError('Произошла ошибка соединения');
-        });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
 
-        xhr.addEventListener('timeout', function () {
-            onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-        });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
 
-        xhr.timeout = TIMEOUT;
+    xhr.timeout = TIMEOUT;
 
-        xhr.open('GET', url);
-        xhr.send();
-    }
+    xhr.open('GET', url);
+    xhr.send();
+  }
 })();
